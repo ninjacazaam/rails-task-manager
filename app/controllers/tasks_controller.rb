8575bccf23199f68c1c7df52b6852f06e7bea853
Corey Ellis <coreypozzatiellis@gmail.com>
2022-06-04 15:33:40 +1000

    refactored with resource routes and set_task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  # before_action :task_params, only: [:create]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -14,22 +14,16 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -37,6 +31,10 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
